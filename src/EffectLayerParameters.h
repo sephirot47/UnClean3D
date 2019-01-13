7a67c3c5efadc6508949ee68da7ecc85f90ed536
sephirot47 <victorantondominguez@gmail.com>
2019-01-13 11:04:48 +0100

    EffectLayer parameters

---
+++ src/EffectLayerParameters.h
@@ -0,0 +1,20 @@
+#ifndef EFFECTLAYERPARAMETERS_H
+#define EFFECTLAYERPARAMETERS_H
+
+#include "Bang/Bang.h"
+#include "Bang/Color.h"
+
+using namespace Bang;
+
+struct EffectLayerParameters
+{
+    float dirtSeed = 0.0f;
+    float dirtFrequency = 4.0f;
+    float dirtFrequencyMultiply = 2.5f;
+    float dirtAmplitude = 1.0f;
+    float dirtAmplitudeMultiply = 0.6f;
+
+    Color tint = Color::Black();
+};
+
+#endif  // EFFECTLAYERPARAMETERS_H
