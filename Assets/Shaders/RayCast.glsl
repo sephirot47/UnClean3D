3dd7598877361f858653e9dfcb87a73c6f560455
sephirot47 <victorantondominguez@gmail.com>
2019-02-17 08:53:32 +0100

    Fixed raycasting

--- Assets/Shaders/RayCast.glsl
+++ Assets/Shaders/RayCast.glsl
@@ -26,15 +26,17 @@ bool RayCast(in vec3 rayOrigin,
     }
 
     vec3 rayDirectionSign = sign(rayDirection);
-    vec3 gridCoordXYZ;
+    vec3[3] rayDirsXYZ;
+    rayDirsXYZ[0] = vec3(1,0,0);
+    rayDirsXYZ[1] = vec3(0,1,0);
+    rayDirsXYZ[2] = vec3(0,0,1);
 
-    int j = 0;
     bool insideGrid = true;
     vec3 currentRayOrigin = rayOrigin;
     float currentMaxDistance = maxDistance;
-    while (++j <= 100 && insideGrid) // && currentMaxDistance > 0)
+    while (insideGrid && currentMaxDistance > 0)
     {
-        gridCoordXYZ = floor( (currentRayOrigin.xyz - GridMinPoint.xyz) / GridCellSize.xyz );
+        vec3 gridCoordXYZ = floor( (currentRayOrigin.xyz - GridMinPoint.xyz) / GridCellSize.xyz );
         insideGrid = (all(greaterThanEqual(gridCoordXYZ, vec3(0))) &&
                       all(lessThan(gridCoordXYZ, vec3(NumGridCells))));
         if (!insideGrid)
@@ -46,9 +48,7 @@ bool RayCast(in vec3 rayOrigin,
         int cellIndex = int(dot(vec3(NC * NC, NC, 1), gridCoordXYZ.zyx));
         int numTriIdsInCell = GetUniformMeshGridArraySize(cellIndex);
         for (int i = 0; i < numTriIdsInCell; ++i)
-        // for (int i = 0; i < 240; ++i)
         {
-            // int triId = i;
             int triId = int(GetUniformMeshGridElement(cellIndex, i).x);
             vec3 triPoints[3];
             for (int vi = 0; vi < 3; ++vi)
@@ -65,61 +65,32 @@ bool RayCast(in vec3 rayOrigin,
             }
         }
 
-        // Find which is the next grid cell we need to go to
-        // vec3 gridStepXYZ = vec3(0);
+        // Advance in ray to the next grid cell
         vec3 cellMin = GridMinPoint + (gridCoordXYZ * GridCellSize);
         vec3 cellCenter = cellMin + GridCellSize * 0.5f;
         {
-            vec3 planeXPoint = cellCenter + vec3(1,0,0) * 0.5f * GridCellSize.x * rayDirectionSign.x;
-            float intDistPlaneX = IntersectRayPlaneDist(rayOrigin, rayDirection,
-                                                        planeXPoint, vec3(1,0,0));
-
-            vec3 planeYPoint = cellCenter + vec3(0,1,0) * 0.5f * GridCellSize.y * rayDirectionSign.y;
-            float intDistPlaneY = IntersectRayPlaneDist(rayOrigin, rayDirection,
-                                                        planeYPoint, vec3(0,1,0));
-
-            vec3 planeZPoint = cellCenter + vec3(0,0,1) * 0.5f * GridCellSize.z * rayDirectionSign.z;
-            float intDistPlaneZ = IntersectRayPlaneDist(rayOrigin, rayDirection,
-                                                        planeZPoint, vec3(0,0,1));
-
             const float INF = 1e12;
-            if (rayDirectionSign.x == 0 || intDistPlaneX < 0) { intDistPlaneX = INF; }
-            if (rayDirectionSign.y == 0 || intDistPlaneY < 0) { intDistPlaneY = INF; }
-            if (rayDirectionSign.z == 0 || intDistPlaneZ < 0) { intDistPlaneZ = INF; }
-
             float minIntersectionDist = INF;
-            if (rayDirectionSign.x != 0 &&
-                intDistPlaneX <= intDistPlaneY &&
-                intDistPlaneX <= intDistPlaneZ)
+            for (int i = 0; i < 3; ++i)
             {
-                minIntersectionDist = intDistPlaneX;
-                // gridStepXYZ = vec3(1,0,0) * rayDirectionSign.x;
-            }
-            else if (rayDirectionSign.y != 0 &&
-                     intDistPlaneY <= intDistPlaneX &&
-                     intDistPlaneY <= intDistPlaneZ)
-            {
-                minIntersectionDist = intDistPlaneY;
-                // gridStepXYZ = vec3(0,1,0) * rayDirectionSign.y;
-            }
-            else if (rayDirectionSign.z != 0 &&
-                     intDistPlaneZ <= intDistPlaneX &&
-                     intDistPlaneZ <= intDistPlaneY)
-            {
-                minIntersectionDist = intDistPlaneZ;
-                // gridStepXYZ = vec3(0,0,1) * rayDirectionSign.z;
-            }
-            else
-            {
-                return false;
+                if (rayDirectionSign[i] != 0)
+                {
+                    vec3 rayDirXYZ = rayDirsXYZ[i];
+                    vec3 planePoint = cellCenter + rayDirXYZ * GridCellSize[i] * rayDirectionSign[i] * 0.5f;
+                    float intDistPlane = IntersectRayPlaneDist(currentRayOrigin, rayDirection,
+                                                                planePoint, rayDirXYZ);
+                    if (intDistPlane >= 0)
+                    {
+                        minIntersectionDist = min(minIntersectionDist, intDistPlane);
+                    }
+                }
             }
 
+            // Advance in grid
             currentMaxDistance -= minIntersectionDist;
-            currentRayOrigin += rayDirection * (minIntersectionDist);
+            currentRayOrigin += rayDirection * (minIntersectionDist + 0.001f);
         }
 
-        // Advance in grid and update whether we are inside
-        // gridCoordXYZ += gridStepXYZ;
     }
 
     return false;
